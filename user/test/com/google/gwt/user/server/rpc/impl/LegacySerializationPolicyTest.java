074300d70ae98ae10e0fec99c91a3e8c7bba92f6
Axel Uhl <axel.uhl@sap.com>
2025-10-31 16:19:38 +0100

    Revert "issue-7987: exclude RuntimeException from non-serializability test;"
    This reverts commit fb0a57ba23b20b501481a4b85df0dbc78fe999f2 which is no
    longer needed when in d3fdeecd4c65abec4dea116cfcc859f2f148e83a we have
    reverted the addition of the RuntimeException_CustomFieldSerializer.

--- user/test/com/google/gwt/user/server/rpc/impl/LegacySerializationPolicyTest.java
+++ user/test/com/google/gwt/user/server/rpc/impl/LegacySerializationPolicyTest.java
@@ -70,15 +70,12 @@ public class LegacySerializationPolicyTest extends TestCase {
         IncompatibleRemoteServiceException.class);
 
     /*
-     * Make sure that the supertypes of IncompatibleRemoteServiceException,
-     * excluding Object and RuntimeException (for the latter we now have a custom field serializer),
-     * should not be serialized as leaf types but their fields
+     * Make sure that the supretypes of IncompatibleRemoteServiceException,
+     * excluding Object, should not be serialized as leaf types but their fields
      * should be serializable.
      */
     for (Class<?> clazz = IncompatibleRemoteServiceException.class.getSuperclass(); clazz != Object.class; clazz = clazz.getSuperclass()) {
-      if (clazz != RuntimeException.class) {
-        assertNotValidSerialize(serializationPolicy, clazz);
-      }
+      assertNotValidSerialize(serializationPolicy, clazz);
       assertSerializeFields(serializationPolicy, clazz);
     }
   }
